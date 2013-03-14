77b48590e0a6ae413db9f89b8c2d2a3a8eacdb70
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2013-03-14 01:53:53 -0400

    Added some more detailed tests, and cleaned up code

--- detile_image.py
+++ detile_image.py
@@ -7,17 +7,6 @@ import numpy as np
 
 from csv import reader
 
-#from re import compile,MULTILINE
-#
-#color_exp=compile(r'^0\s+!COLOU?R\s+(\w+)\s+CODE\s+(\d+)\s+VALUE\s+#([A-Fa-f0-9]{6})\s+EDGE\s+\#(\d+|[A-Fa-f0-9]{6}).*?$',MULTILINE)
-#
-#def parse_color(fname):
-#	colors = []
-#	with open(fname,'r') as fhandle:
-#		fdata=fhandle.read()
-#		colors=color_exp.findall(fdata)
-#	return colors
-
 epsilon = np.finfo(np.float32).eps
 
 simple_ufunc = lambda f:np.frompyfunc(f,1,1)
@@ -30,15 +19,8 @@ def itofcolor(color):
 def ftoicolor(color):
 	return int(color*255)
 
-#@np.vectorize
-#def getnth(v,n):
-#	return v[n]
-
-def nargmax(myarray, n):
-	#print n,myarray
-	
-	ind = np.argsort(myarray)
-	return ind[-n:][::-1]#, myarray[ind[-n:]]
+def nargmax(npary, n):
+	return np.argsort(npary)[-n:][::-1]
 
 def strider(npary):
 	@simple_ufunc
@@ -58,10 +40,9 @@ def make_palette(fname):
 			colors[int(row[1])] = array((parse_color(row[2]),parse_color(row[3]),array((float(row[4])/255,float(row[5]),0))))
 	return colors
 
-
-#def find_nearest(difs)
-
 def optimize_palette(palette):
+	# Remove transparent/luminant colors,
+	# because we don't have any in our inputs, for now
 	global epsilon
 	to_filt=transpose(array((arange(512),palette[:,0,0],palette[:,0,1],palette[:,0,2],palette[:,2,0],palette[:,2,1])))
 	filt_obj=(to_filt[:,1]>=0) * (np.abs(to_filt[:,4]-1)<(100*epsilon)) * (np.abs(to_filt[:,5])<(100*epsilon))
@@ -74,13 +55,13 @@ def optimize_palette(palette):
 def palettize(image,palette):
 	print "\tOptimizing palette (palettize)"
 	find_nearest=optimize_palette(palette)
-	#print "\tInitializing floating point colors"
-	#npimg=itofcolor(array(image))
 	print "\tColor mapping"
 	return np.apply_along_axis(find_nearest,-1,itofcolor(array(image)))
 
 def get_vector_fetch(palette):
-	#@simple_ufunc
+	# index is actually an array of length 3
+	# to make sure our output is an array n x m x 3 containing scalars
+	# instead of an array of n x m containing length-3 arrays of scalars
 	def fetch_color(index):
 		return palette[index[0],0]
 	return fetch_color
@@ -89,22 +70,14 @@ def get_vector_fetch(palette):
 def depalettize(indexed,palette):
 	width,height=indexed.shape
 	fetch_color = get_vector_fetch(palette)
-	#ret = zeros((width,height,3))
 	return ftoicolor(np.apply_along_axis(fetch_color,-1,indexed[:,:,np.newaxis])).astype(uint8)
-	#ret[:,:,0] = getnth(recolored[:,:],0)
-	#ret[:,:,1] = getnth(recolored[:,:],1)
-	#ret[:,:,2] = getnth(recolored[:,:],2)
-	#print "\tCasting to int"
-	##ret = ftoicolor(ret).astype(uint8)
-	#ret = ftoicolor(recolored).astype(uint8)
-	
 
 def display_grids(*surfs):
 	if 'Axes3D' not in globals() or 'plt' not in globals():
 		from mpl_toolkits.mplot3d import Axes3D
 		import matplotlib.pyplot as plt
 	for name,surf,(xo,xs,yo,ys) in surfs:
-		fig = plt.figure() # too old to do subplots correctly!
+		fig = plt.figure() # too old to do subplots correctly! put this outside for loop if we update
 		ax = fig.add_subplot(111, projection='3d')
 		
 		height,width=surf.shape
@@ -113,7 +86,6 @@ def display_grids(*surfs):
 		y= arange(yo,height,ys)
 		X, Y = np.meshgrid(x, y)
 		
-		#print X.shape,Y.shape,surf.shape
 		ax.plot_surface(X, Y, surf[yo::ys,xo::xs],cmap='spectral')
 		#ax.set_zlim(*(np.mean(surf) + array([-3,3])*np.std(surf)))
 		
@@ -121,7 +93,7 @@ def display_grids(*surfs):
 		ax.set_ylabel('Y Label')
 		ax.set_zlabel(name)
 
-		plt.show()
+		plt.show() # de-indent this if you go back to subplots
 	
 
 def add_suffix(basename,suffix):
@@ -131,6 +103,7 @@ def add_suffix(basename,suffix):
 
 
 def main(*args):
+	print "--------===",args[1],"===--------"
 	print "Making palette"
 	palette= make_palette(args[0])
 	
@@ -155,10 +128,7 @@ def main(*args):
 	oimg=Image.fromarray(rgbalized)
 	#oimg.show()
 	oimg.save(add_suffix(basename,"palettized"),quality=100)
-	print "Done"
-	
-	print
-	print
+
 	print "Fourier Analysis"
 	four = fft2(indexed)
 	#print "Displaying"
@@ -173,7 +143,6 @@ def main(*args):
 	print "Min tiling resolution",min_x_tile,"x",min_y_tile
 	
 	max_y_freq, max_x_freq = indexed.shape
-	#print max_x_freq,max_y_freq
 	
 	xmeans = np.mean(afour,axis=0)[:1+max_x_freq/2]
 	ymeans = np.mean(afour,axis=1)[:1+max_y_freq/2]
@@ -192,26 +161,19 @@ def main(*args):
 	xmaxima = ((np.diff(np.sign(np.diff(xmmeans))) < 0).nonzero()[0] + 1)
 	ymaxima = ((np.diff(np.sign(np.diff(ymmeans))) < 0).nonzero()[0] + 1)
 	
-	#print xmaxima
-	#print ymaxima
-	#print
-	
-	#print np.logical_not(xmaxima[:,np.newaxis] % kxrange[1:]).astype(uint8)
-	#print np.logical_not(ymaxima[:,np.newaxis] % kyrange[1:]).astype(uint8)
 	print "Suggested tiling schemes"
-	print "X:",1+nargmax(np.apply_along_axis(np.sum,0,np.logical_not(xmaxima[:,np.newaxis] % kxrange[1:])),4)
-	print "Y:",1+nargmax(np.apply_along_axis(np.sum,0,np.logical_not(ymaxima[:,np.newaxis] % kyrange[1:])),4)
-	#
-	#
-	#print 2+np.argmax(np.apply_along_axis(np.sum,0,np.logical_not(xmaxima[:,np.newaxis] % kxrange[1:])))
-	#print 2+np.argmax(np.apply_along_axis(np.sum,0,np.logical_not(ymaxima[:,np.newaxis] % kyrange[1:])))
+	x_suggest = 1+nargmax(np.apply_along_axis(np.sum,0,np.logical_not(xmaxima[:,np.newaxis] % kxrange[1:])),2)
+	y_suggest = 1+nargmax(np.apply_along_axis(np.sum,0,np.logical_not(ymaxima[:,np.newaxis] % kyrange[1:])),2)
+	print "X:",x_suggest,xmmeans[x_suggest]
+	print "Y:",y_suggest,ymmeans[y_suggest]
 	
-	#print np.apply_along_axis(np.mean,-1,xstrider(kxrange[1:])[:,np.newaxis])
-	#print kxrange[::kxrange]
-	#print kyrange[::kyrange]
+	print "Best-guess scheme"
+	print "X:",x_suggest[np.argmax(xmmeans[x_suggest])]
+	print "Y:",y_suggest[np.argmax(ymmeans[y_suggest])]
+	
 	
 	print "Done"
-	
+	print
 	
 if __name__=="__main__":
 	import sys
--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+./detile_image.py ldconfig.csv xelnagatiles5x3.jpg 194 150 1 1
+./detile_image.py ldconfig.csv xelnagatiles.jpg 78 100 1 1
+./detile_image.py ldconfig.csv xelnaga.jpg 39 50 1 1
+
+
 ./detile_image.py ldconfig.csv xelnagatiles5x3.jpg 194 150 2 2
 ./detile_image.py ldconfig.csv xelnagatiles.jpg 78 100 2 2
 ./detile_image.py ldconfig.csv xelnaga.jpg 39 50 2 2
+
+
+./detile_image.py ldconfig.csv xelnagatiles5x3.jpg 194 150 3 3
+./detile_image.py ldconfig.csv xelnagatiles.jpg 78 100 3 3
+./detile_image.py ldconfig.csv xelnaga.jpg 39 50 3 3
+
+
+
+./detile_image.py ldconfig.csv header_fbdude.png 80 60 1 1
+./detile_image.py ldconfig.csv header_fbdude-stretched-middle.jpg 200 60 1 1
+./detile_image.py ldconfig.csv header_fbdude-weirdx2.jpg 300 120 1 1
+
+
+./detile_image.py ldconfig.csv header_fbdude.png 80 60 2 2
+./detile_image.py ldconfig.csv header_fbdude-stretched-middle.jpg 200 60 2 2
+./detile_image.py ldconfig.csv header_fbdude-weirdx2.jpg 300 120 2 2
+
+
+./detile_image.py ldconfig.csv header_fbdude.png 80 60 3 3
+./detile_image.py ldconfig.csv header_fbdude-stretched-middle.jpg 200 60 3 3
+./detile_image.py ldconfig.csv header_fbdude-weirdx2.jpg 300 120 3 3
